522c97b81317f39aecb7e3e3c115cfc89158475b
Sheila Tejada <sheymile@gmail.com>
2017-10-15 08:54:49 -0400

    Bloque

---
+++ core/bloque_integrante.php
@@ -0,0 +1,12 @@
+<div class="bloque_tutor">
+				<img src="imagenes/image_sesion.png">
+				<div class="tutor" style="display: inline-block; padding-left: 30px;">
+					<span class="extra_peque normal claro">tutor</span><br>
+					
+					<?php echo "<span class='grande negrita oscuro'>".$datos["nombre"] . " " . $datos["apellido"] . " </span><br>"?>
+					<span class="extra_peque normal claro"><a href="salir.php">Salir de la sesión</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href="#">Eliminar sesión</a></span>
+				</div>
+				<div class="hr">
+					<hr>
+				</div>
+			</div>
