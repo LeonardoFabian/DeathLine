f3cf288e013f23fe0e236df53c11b8b254dd12a1
Jonas299 <32756183+Jonas299@users.noreply.github.com>
2017-10-14 23:07:51 -0400

    Add files via upload

---
+++ cerro_sesion.php
@@ -0,0 +1,28 @@
+<style type="text/css">
+.session_out{
+
+	margin-top: 25px;
+	overflow: visible;
+	border: 2px solid black;
+	width: 400px;
+}
+img{
+	width: 130px;
+	margin-bottom: 15px;
+}
+a{
+
+	text-decoration: none;
+	color: orange;
+	font-size: 1.6em;
+	
+}
+</style>
+<center>
+<div class = "session_out">
+	<h2>Uy!! parece que no ha iniciado sesión</h2>
+<img src="imagenes/SesionOut.png">
+<br/>
+<a href="index.php">Iniciar sesion</a>
+</div>
+<center/>
